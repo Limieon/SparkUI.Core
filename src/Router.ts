1879dade7b208fa5442e894a99e94594b8816e18
Justin Jakob <justin.jakob391@outlook.com>
2024-11-20 02:57:45 +0100

    Improved router syntax

--- src/Router.ts
+++ src/Router.ts
@@ -1,67 +1,237 @@
 import FS from 'fs'
 import Path from 'path'
-import Express, {
-    RequestHandler,
-    Router,
-    type NextFunction,
-    type Request,
-    type Response,
-} from 'express'
+import * as Express from 'express'
 import cookieParser from 'cookie-parser'
 import * as Env from '@env'
-import Chalk from 'chalk'
+import Chalk, { ChalkInstance } from 'chalk'
 
 import Logger from '@log'
+import { ZodError, ZodSchema } from 'zod'
+import { authMiddleware, JWTPayload } from './service/Auth'
 
-const router = Express()
-router.use(cookieParser(Env.SPARKUI_CORE_COOKIE_SECRET))
-router.use(Express.json())
+const handler = Express.default()
+handler.use(cookieParser(Env.SPARKUI_CORE_COOKIE_SECRET))
+handler.use(Express.json())
 
 const methodColors: { [key: string]: ChalkInstance } = {
-    GET: Chalk.magenta,
-    POST: Chalk.green,
-    PUT: Chalk.yellow,
-    DELETE: Chalk.red,
-    PATCH: Chalk.yellowBright,
-    OPTIONS: Chalk.blue,
-    HEAD: Chalk.blueBright,
+	GET: Chalk.magenta,
+	POST: Chalk.green,
+	PUT: Chalk.yellow,
+	DELETE: Chalk.red,
+	PATCH: Chalk.yellowBright,
+	OPTIONS: Chalk.blue,
+	HEAD: Chalk.blueBright,
 }
 
-const routeLogger: RequestHandler = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    const color = methodColors[req.method] || Chalk.white
-    Logger.debug(
-        `${color(req.method)}${' '.repeat(7 - req.method.length)} ${req.url}`
-    )
-    next()
+const routeLogger: Express.RequestHandler = async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+	const color = methodColors[req.method] || Chalk.white
+	Logger.debug(`${color(req.method)}${' '.repeat(7 - req.method.length)} ${req.url}`)
+	next()
 }
 
-if (Env.SPARKUI_CORE_DEBUG) router.use(routeLogger)
+if (Env.SPARKUI_CORE_DEBUG) handler.use(routeLogger)
 
 export async function initRoutes(dir: string, prefix: string) {
-    async function initSubRoutes(path: string) {
-        for (let f of FS.readdirSync(path)) {
-            const item = Path.join(path, f)
-            if (FS.lstatSync(item).isDirectory()) {
-                await initSubRoutes(Path.join(path, f))
-                continue
-            }
+	async function initSubRoutes(path: string) {
+		for (let f of FS.readdirSync(path)) {
+			const item = Path.join(path, f)
+			if (FS.lstatSync(item).isDirectory()) {
+				await initSubRoutes(Path.join(path, f))
+				continue
+			}
 
-            const routerPrefix = Path.join(
-                prefix,
-                Path.relative(dir, path)
-            ).replaceAll('\\', '/')
-            Logger.debug('Loading route', routerPrefix)
-            router.use(routerPrefix, (await import(item)).default as Router)
-        }
-    }
+			const routerPrefix = Path.join(prefix, Path.relative(dir, path)).replaceAll('\\', '/')
+			Logger.debug('Loading route', routerPrefix)
+			const data = await import(item)
+			if (data.default instanceof Router) {
+				data.default.register()
+			} else {
+				handler.use(routerPrefix, (await import(item)).default as Express.Router)
+			}
+		}
+	}
 
-    await initSubRoutes(dir)
+	await initSubRoutes(dir)
 }
 
-const server = router.listen(Env.SPARKUI_CORE_PORT, () => {
-    Logger.info(`Server listening on port ${Env.SPARKUI_CORE_PORT}`)
+const server = handler.listen(Env.SPARKUI_CORE_PORT, () => {
+	Logger.info(`Server listening on port ${Env.SPARKUI_CORE_PORT}`)
 })
+
+interface RequestHandlerParams<Q> {
+	handle: Express.Request
+	query: Q
+}
+interface AuthRequestHandlerParams<Q> {
+	handle: Express.Request
+	query: Q
+	user: JWTPayload
+}
+
+interface BodyRequestHandlerParams<Q, B> extends RequestHandlerParams<Q> {
+	body: B
+}
+interface BodyAuthRequestHandlerParams<Q, B> extends AuthRequestHandlerParams<Q> {
+	body: B
+}
+
+interface RequestHandlerReturn {
+	headers?: { [key: string]: string }
+	cookies?: { [key: string]: string }
+	contentType?: string
+	body?: any
+	status: number
+}
+
+type RequestHandler<Q> = (req: RequestHandlerParams<Q>) => Promise<RequestHandlerReturn>
+type AuthRequestHandler<Q> = (req: AuthRequestHandlerParams<Q>) => Promise<RequestHandlerReturn>
+
+type BodyRequestHandler<Q, B> = (req: BodyRequestHandlerParams<Q, B>) => Promise<RequestHandlerReturn>
+type BodyAuthRequestHandler<Q, B> = (req: BodyAuthRequestHandlerParams<Q, B>) => Promise<RequestHandlerReturn>
+
+export default class Router {
+	constructor(prefix: string, secure: boolean = true) {
+		this.prefix = prefix
+		this.router = Express.Router()
+		this.router.use(authMiddleware)
+	}
+
+	private async getRequestData<Q>(req: Express.Request, schema: ZodSchema<Q>, handler: RequestHandler<Q>) {
+		const query = schema.parse({ ...req.query, ...req.params })
+		const response = await handler({ handle: req, query })
+		return {
+			response,
+			contentType: this.getContentType(response),
+		}
+	}
+	private async getRequestDataSecure<Q>(req: Express.Request, schema: ZodSchema<Q>, handler: AuthRequestHandler<Q>) {
+		const query = schema.parse({ ...req.query, ...req.params })
+		const response = await handler({ handle: req, query, user: req.user! })
+		return {
+			response,
+			user: req.user,
+			contentType: this.getContentType(response),
+		}
+	}
+
+	private async getRequestDataWithBody<Q, B>(
+		req: Express.Request,
+		querySchema: ZodSchema<Q>,
+		bodySchema: ZodSchema<B>,
+		handler: BodyRequestHandler<Q, B>
+	) {
+		const query = querySchema.parse({ ...req.query, ...req.params })
+		const body = bodySchema.parse(req.body)
+		const response = await handler({ handle: req, query, body })
+		return {
+			response,
+			contentType: this.getContentType(response),
+			body,
+		}
+	}
+	private async getRequestDataWithBodySecure<Q, B>(
+		req: Express.Request,
+		querySchema: ZodSchema<Q>,
+		bodySchema: ZodSchema<B>,
+		handler: BodyAuthRequestHandler<Q, B>
+	) {
+		const query = querySchema.parse({ ...req.query, ...req.params })
+		const body = bodySchema.parse(req.body)
+		const response = await handler({ handle: req, query, user: req.user!, body })
+		return {
+			response,
+			contentType: this.getContentType(response),
+			body,
+		}
+	}
+
+	private returnError(res: Express.Response, e: any) {
+		if (e instanceof ZodError) {
+			res.status(400).json({ error: e.message, success: false })
+		} else {
+			Logger.error(e)
+
+			if (Env.SPARKUI_CORE_DEBUG) {
+				res.status(500).json({ error: e.message, stack: e.stack })
+			} else {
+				res.status(500).json({ error: 'Internal server error' })
+			}
+		}
+	}
+	private parseResponse(contentType: string, res: Express.Response, response: RequestHandlerReturn) {
+		res.status(response.status)
+
+		if (response.headers) {
+			for (let key in response.headers) {
+				res.header(key, response.headers[key])
+			}
+		}
+		res.contentType(contentType)
+
+		if (response.cookies) {
+			for (let key in response.cookies) {
+				res.cookie(key, response.cookies[key])
+			}
+		}
+
+		res.send(response.body)
+	}
+	private getContentType(response: RequestHandlerReturn) {
+		return response.contentType
+			? response.contentType
+			: response.body instanceof Buffer
+			? 'application/octet-stream'
+			: response.body instanceof Object
+			? 'application/json'
+			: 'text/plain'
+	}
+
+	get<Q = {}>(path: string, querySchema: ZodSchema<Q>, handler: AuthRequestHandler<Q>) {
+		Logger.debug('Registering GET', path)
+		this.router.get(path, async (req, res) => {
+			try {
+				const user = req.user
+				if (!user) {
+					res.status(401).json({ error: 'Unauthorized' })
+					return
+				}
+
+				const { response, contentType } = await this.getRequestDataSecure(req, querySchema, handler)
+				this.parseResponse(contentType, res, response)
+			} catch (e) {
+				this.returnError(res, e)
+			}
+		})
+	}
+
+	post<Q = {}, B = {}>(path: string, querySchema: ZodSchema<Q>, bodySchema: ZodSchema<B>, handler: BodyAuthRequestHandler<Q, B>) {
+		Logger.debug('Registering POST', path)
+
+		this.router.post(path, async (req, res) => {
+			try {
+				const user = req.user
+				if (!user) {
+					res.status(401).json({ error: 'Unauthorized' })
+					return
+				}
+
+				const { response, contentType } = await this.getRequestDataWithBodySecure(req, querySchema, bodySchema, handler)
+				this.parseResponse(contentType, res, response)
+			} catch (e) {
+				this.returnError(res, e)
+			}
+		})
+	}
+
+	register() {
+		Logger.debug('Registering router', this.prefix)
+		handler.use(this.prefix, this.router)
+	}
+
+	use(path: string, handler: Express.RequestHandler) {
+		this.router.use(Path.join(this.prefix, path).replaceAll('\\\\', '/'), handler)
+	}
+
+	prefix: string
+	router: Express.Router
+}
--- src/routes/api/v1/image/index.ts
+++ src/routes/api/v1/image/index.ts
@@ -1,5 +1,5 @@
 import { authMiddleware } from '$/service/Auth'
-import { Router, Request, Response } from 'express'
+import Router from '$/Router'
 
 import { UserSchema } from '$/routes/api/v1/user'
 
@@ -7,91 +7,71 @@ import * as Table from '@db/schema'
 
 import z, { ZodError } from 'zod'
 import db from '$/db'
-import { eq } from 'drizzle-orm'
+import { and, eq } from 'drizzle-orm'
+import Logger from '@log'
 
-const router = Router()
-router.use(authMiddleware)
+//const router = Router()
+//router.use(authMiddleware)
 
 export const ImageSchema = z.object({
-    id: z.string().uuid(),
-    blurHash: z.string(),
-    createdAt: z.number(),
+	id: z.string().uuid(),
+	blurHash: z.string(),
+	createdAt: z.number(),
 })
 export const RefImageSceham = z.object({
-    id: z.string().uuid(),
+	id: z.string().uuid(),
 })
 export type ImageType = z.infer<typeof ImageSchema>
 export type RefImageType = z.infer<typeof RefImageSceham>
 
-router.get('', async (req: Request, res: Response) => {
-    const QueryParams = z.object({})
-    type QueryType = z.infer<typeof QueryParams>
-    const query: QueryType = QueryParams.parse({ ...req.query, ...req.params })
+const router = new Router('/api/v1/image')
+router.get('/', z.object({}), async (req) => {
+	const user = req.user
+	const images = await db
+		.select({
+			id: Table.Image.id,
+			createdAt: Table.Image.createdAt,
+			blurHash: Table.Image.blurHash,
+		})
+		.from(Table.Image)
+		.where(eq(Table.Image.creatorId, user.sub))
 
-    const user = req.user
-    if (!user) {
-        res.status(404).json({ error: 'User not found' })
-        return
-    }
+	const data = images.map((e) => ({
+		id: e.id,
+		blurHash: e.blurHash,
+		createdAt: e.createdAt,
+	}))
 
-    try {
-        const images = await db
-            .select({
-                id: Table.Image.id,
-                createdAt: Table.Image.createdAt,
-                blurHash: Table.Image.blurHash,
-            })
-            .from(Table.Image)
-            .where(eq(Table.Image.creatorId, user.sub))
-
-        const data = images.map((e) => ({
-            id: e.id,
-            blurHash: e.blurHash,
-            createdAt: e.createdAt,
-        }))
-
-        res.status(200).json({ data, meta: {} })
-    } catch (e) {
-        if (e instanceof ZodError) {
-            res.status(400).json({ error: e.message })
-        }
-        res.status(500).json({ error: e.message })
-    }
+	return {
+		status: 200,
+		body: { data, meta: {} },
+	}
 })
 
-router.get('/:id/raw.webp', async (req: Request, res: Response) => {
-    const QueryParams = z.object({
-        id: z.string().uuid(),
-    })
-    type QueryType = z.infer<typeof QueryParams>
-    const query: QueryType = QueryParams.parse({ ...req.query, ...req.params })
+router.get(
+	'/:id/raw.webp',
+	z.object({
+		id: z.string().uuid(),
+	}),
+	async (req) => {
+		const image = await db
+			.select({ data: Table.Image.data })
+			.from(Table.Image)
+			.where(and(eq(Table.Image.id, req.query.id), eq(Table.Image.creatorId, req.user.sub)))
+			.limit(1)
 
-    const user = req.user
-    if (!user) {
-        res.status(404).json({ error: 'User not found' })
-        return
-    }
+		if (image.length < 1)
+			return {
+				status: 404,
+				body: { error: 'Image not found' },
+			}
 
-    try {
-        const image = await db
-            .select({ data: Table.Image.data })
-            .from(Table.Image)
-            .where(eq(Table.Image.id, query.id))
-            .limit(1)
-
-        if (image.length < 1) {
-            res.status(404).json({ error: 'Image not found' })
-            return
-        }
-
-        res.setHeader('Content-Type', 'image/webp')
-        res.status(200).send(image[0].data)
-    } catch (e) {
-        if (e instanceof ZodError) {
-            res.status(400).json({ error: e.message })
-        }
-        res.status(500).json({ error: e.message })
-    }
-})
+		return {
+			status: 200,
+			contentType: 'image/webp',
+			body: image[0].data,
+		}
+	}
+)
 
 export default router
